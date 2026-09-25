65716960685c35b4c2cda05ee974c227217c5e3a
Hansraj Meena <hans004333@gmail.com>
2026-09-25 13:12:13 +0530

    Fix message merge test fixture

--- android/app/src/test/java/com/chatflow/app/MessageMergeTest.kt
+++ android/app/src/test/java/com/chatflow/app/MessageMergeTest.kt
@@ -103,8 +103,10 @@ class MessageMergeTest {
             message_type = "text",
             content = "Hello",
             reply_to_message_id = null,
+            forwarded_from_message_id = null,
             created_at = createdAt,
             edited_at = null,
-            deleted_at = null
+            deleted_at = null,
+            expires_at = null
         )
 }
